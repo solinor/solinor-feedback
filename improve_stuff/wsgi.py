2d29fc99ee4e9a562de453ba53b13144afbcd61c
Olli Jarva <olli.jarva@solinor.fi>
2018-01-28 19:29:37 +0200

    Add sentry

--- improve_stuff/wsgi.py
+++ improve_stuff/wsgi.py
@@ -1,9 +1,11 @@
 import os
 
 from django.core.wsgi import get_wsgi_application  # pylint: disable=wrong-import-position
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 from whitenoise.django import DjangoWhiteNoise  # pylint: disable=wrong-import-position
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "improve_stuff.settings")
 
 application = get_wsgi_application()  # pylint: disable=invalid-name
+application = Sentry(application)  # pylint: disable=invalid-name
 application = DjangoWhiteNoise(application)  # pylint: disable=invalid-name
